11f8cdc9b15113b982af2c52deda456d7b31d9a5
jessi87 <jessiseo87@gmail.com>
2025-12-16 17:29:34 +0900

    init commit

---
+++ src/test/java/com/user/awstest/AwsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.awstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
